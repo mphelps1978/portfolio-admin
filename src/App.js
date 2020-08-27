38663cf66703b5830733ff381ffa1e7c056ab77e
Michael Phelps <michaeljphelps1978@gmail.com>
2020-08-27 16:34:18 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import Resume from "./components/resume/Resume";
+import Projects from "./components/projects/Projects";
+import Home from "./components/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/resume">Dashboard</Link>
+          </li>
+        </ul>
+        <hr />
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/resume">
+          <Resume />
+        </Route>
+      </Switch>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/resume/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div>
+      <h1>Hello from Resumes</h1>
+    </div>
+  )
+}
+
+export default Resume
